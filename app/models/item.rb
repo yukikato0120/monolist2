6e9140b50016520807b44e31af5559d547e64cab
yukikato0120 <y-kato@grow-up1.co.jp>
2016-07-11 07:05:10 +0000

    work2

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,4 +1,8 @@
 class Item < ActiveRecord::Base
   has_many :ownerships  , foreign_key: "item_id" , dependent: :destroy
   has_many :users , through: :ownerships
+  has_many :wants , foreign_key: "item_id"
+  has_many :want_users , source: :users
+  has_many :have_users , foreign_key: "item_id"
+  has_many :have_users , source: :users
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -6,7 +6,7 @@ class User < ActiveRecord::Base
                     format: { with: VALID_EMAIL_REGEX },
                     uniqueness: { case_sensitive: false }
   has_secure_password
-  
+
   has_many :following_relationships, class_name:  "Relationship", foreign_key: "follower_id", dependent: :destroy
   has_many :following_users, through: :following_relationships, source: :followed
   has_many :followed_relationships, class_name:  "Relationship", foreign_key: "followed_id", dependent: :destroy
@@ -14,7 +14,10 @@ class User < ActiveRecord::Base
 
   has_many :ownerships , foreign_key: "user_id", dependent: :destroy
   has_many :items ,through: :ownerships
-
+  has_many :wants, class_name: "Want", foreign_key: "user_id", dependent: :destroy
+  has_many :want_items , through: :wants, source: :item
+  has_many :haves, class_name: "Have", foreign_key: "user_id", dependent: :destroy
+  has_many :have_items , through: :haves, source: :item
 
   # 他のユーザーをフォローする
   def follow(other_user)
@@ -31,20 +34,28 @@ class User < ActiveRecord::Base
 
   ## TODO 実装
   def have(item)
+    have_items.find_or_create_by(user_id: other_user_id)
   end
 
   def unhave(item)
+    have_items=have_items.find_by(user_id: other_user.id)
+    have_items if have_items
   end
 
   def have?(item)
+    have_items.include?(user_id)
   end
 
   def want(item)
+    want_items.find_or_create_by(wants_id: other_user.id)
   end
 
   def unwant(item)
+    want_items=want_items.find_by(user_id: other_user.id)
+    want_items if want_items
   end
 
   def want?(item)
+    want_items.include?(user_id)
   end
 end
