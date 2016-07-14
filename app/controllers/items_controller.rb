3b84c262ab5b47e625d267789602958e1bb2660a
yukikato0120 <y-kato@grow-up1.co.jp>
2016-07-14 03:49:53 +0000

    work user_item

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -13,7 +13,6 @@ class ItemsController < ApplicationController
   end
 
   def show
-    @item = Item.find(params[:id])
     @have_users = @item.have_users
     @want_users = @item.want_users
   end
--- app/models/item.rb
+++ app/models/item.rb
@@ -1,8 +1,19 @@
 class Item < ActiveRecord::Base
   has_many :ownerships  , foreign_key: "item_id" , dependent: :destroy
   has_many :users , through: :ownerships
-  has_many :wants , foreign_key: "item_id"
-  has_many :want_users , source: :users
-  has_many :have_users , foreign_key: "item_id"
-  has_many :have_users , source: :users
+
+  has_many :wants, class_name: "Want", foreign_key: "item_id", dependent: :destroy
+  has_many :want_users , through: :wants, source: :user
+
+  has_many :haves, class_name: "Have", foreign_key: "item_id", dependent: :destroy
+  has_many :have_users , through: :haves, source: :user
+
+#　初期にあったコマンド
+#  has_many :ownerships  , foreign_key: "item_id" , dependent: :destroy
+#  has_many :users , through: :ownerships
+#  has_many :wants , foreign_key: "item_id"
+#  has_many :want_users , source: :users
+#  has_many :have_users , foreign_key: "item_id"
+#  has_many :have_users , source: :users
+
 end
